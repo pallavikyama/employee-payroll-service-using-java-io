b281814d0ec169d3afda5d46fc2de5f7e28f3f16
pallavikyama <kyamap98@gmail.com>
2020-11-01 14:35:10 +0530

    Added: MySQL-JDBC driver is added to the gradle project. Java program has successfully established connection with the database.

---
+++ main/java/com/blz/employeepayrolljavaio/DemoJDBC.java
@@ -0,0 +1,37 @@
+package com.blz.employeepayrolljavaio;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DemoJDBC {
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "Pallavi@6855";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database:" + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successful!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = driverList.nextElement();
+			System.out.println("\t" + driverClass.getClass().getName());
+		}
+	}
+}
