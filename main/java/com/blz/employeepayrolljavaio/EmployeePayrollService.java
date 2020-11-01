f9130cce76b4e910b5cf91a63435b9ff40d2455b
pallavikyama <kyamap98@gmail.com>
2020-11-01 11:23:38 +0530

    Added: Welcome message is added.

---
+++ main/java/com/blz/employeepayrolljavaio/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.blz.employeepayrolljavaio;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Service!");
+	}
+}
