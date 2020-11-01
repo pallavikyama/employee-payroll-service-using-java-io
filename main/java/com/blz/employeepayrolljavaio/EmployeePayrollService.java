a5630cbd3fdfe8a41aa6e2bde27867a1cb319b88
pallavikyama <kyamap98@gmail.com>
2020-11-01 14:05:05 +0530

    Modified: Printed employee payroll data onto the console line by line.

--- main/java/com/blz/employeepayrolljavaio/EmployeePayrollService.java
+++ main/java/com/blz/employeepayrolljavaio/EmployeePayrollService.java
@@ -46,4 +46,9 @@ public class EmployeePayrollService {
 			return new EmployeePayrollFileIOService().countEntries();
 		return 0;
 	}
+
+	public void printData(IOService ioService) {
+		if (ioService.equals(EmployeePayrollService.IOService.FILE_IO))
+			new EmployeePayrollFileIOService().printData();
+	}
 }
--- test/java/com/blz/employeepayrolljavaio/EmployeePayrollServiceTest.java
+++ test/java/com/blz/employeepayrolljavaio/EmployeePayrollServiceTest.java
@@ -9,12 +9,13 @@ import org.junit.Test;
 public class EmployeePayrollServiceTest {
 
 	@Test
-	public void givenThreeEmployees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+	public void givenThreeEmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
 		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
 				new EmployeePayrollData(2, "Bill Gates", 200000.0),
 				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
 		employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
 		long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
 		assertEquals(3, entries);
 	}
